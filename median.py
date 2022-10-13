94b791105bcd4b812d87cdce7c2bd28d30f67a17
Mathew Tran <m-mathew@hotmail.co.uk>
2022-10-13 17:52:19 +0100

    Median v3

--- median.py
+++ median.py
@@ -6,7 +6,7 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
-
+        numbers.sort()
         lnum = len(numbers)
         midnum = len(numbers) // 2
         if lnum % 2 == 0:
