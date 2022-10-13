2e982a80bf0c81a9e760724bf743cf5a59a14eba
Mathew Tran <m-mathew@hotmail.co.uk>
2022-10-13 17:06:17 +0100

    Median Task

--- median.py
+++ median.py
@@ -1,12 +1,22 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
+from statistics import median
+
 
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+
+        lnum = len(numbers)
+        midnum = len(numbers) // 2
+        if lnum % 2 == 0:
+            mednum = (numbers[midnum-1] + numbers[midnum]) / 2
+
+        elif lnum % 2 == 1:
+            mednum = numbers[midnum]
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+print(mednum)
